b082f0f951ce5e3ec6b4116ddbe35039bfed624e
Владислав <vlados@MacBook-Pro-Vladislav.local>
2020-10-27 15:54:13 +0300

    on schedule

---
+++ src/assets/styles/components/yandex/yandex.js
@@ -0,0 +1,51 @@
+let services = [
+    {
+        backgroundImage: 'url(//yastatic.net/s3/home/services/block/video.svg)',
+        title: 'Видео'
+    },
+    {
+        backgroundImage: 'url(//yastatic.net/s3/home/services/block/kartinki.svg)',
+        title: 'Картинки'
+    },
+    {
+        backgroundImage: 'url(//yastatic.net/s3/home/services/block/news.svg)',
+        title: 'Новости'
+    },
+    {
+        backgroundImage: 'url(//yastatic.net/s3/home/services/block/maps.svg)',
+        title: 'Карты'
+    },
+    {
+        backgroundImage: 'url(//yastatic.net/s3/home/services/block/market_new.svg)',
+        title: 'Маркет'
+    },
+    {
+        backgroundImage: 'url(//yastatic.net/s3/home/services/block/translate.svg)',
+        title: 'Переводчик'
+    },
+    {
+        backgroundImage: 'url(//yastatic.net/s3/home/services/block/music1.svg)',
+        title: 'Музыка'
+    },
+    {
+        backgroundImage: 'url(//yastatic.net/s3/home/services/block/tv.svg)',
+        title: 'Программа'
+    },
+    {
+        backgroundImage: 'url(//yastatic.net/s3/home/services/block/autoru.svg)',
+        title: 'Авто.ру'
+    },
+    {
+        backgroundImage: 'url(//yastatic.net/s3/home/services/block/kartinki.svg)',
+        title: 'ещё'
+    },
+]
+for(let i = 0; i < services.length; i++){
+    let serviceItem = `<div class="services__item">
+                            <div class="services__image" style="background-image: ${services[i].backgroundImage}"></div>
+                            <div class="services__title">${services[i].title}</div>
+                        </div>`
+    let serviceParent = document.getElementsByClassName('services__container')[0];
+    serviceParent.insertAdjacentHTML('beforeend', serviceItem);
+}
+
